a82afd6d517b183d0ff6d6b02995e35f34ed1747
luyuan_mac <luyuan_mac@luyuan-mactekiMac-mini.local>
2013-04-17 16:01:54 +0800

    Revert "Displaying Information in the Detail Scene"
    This reverts commit 501ccd84330a9b56efc50fd1a3f45ea9f2ea1cc4.

--- BirdWatching/BirdsDetailViewController.m
+++ BirdWatching/BirdsDetailViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "BirdsDetailViewController.h"
-#import "BirdSighting.h"
 
 @interface BirdsDetailViewController ()
 @property (strong, nonatomic) UIPopoverController *masterPopoverController;
@@ -18,10 +17,10 @@
 
 #pragma mark - Managing the detail item
 
-- (void)setSighting:(BirdSighting *) newSighting
+- (void)setDetailItem:(id)newDetailItem
 {
-    if (_sighting != newSighting) {
-        _sighting = newSighting;
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
         
         // Update the view.
         [self configureView];
@@ -36,18 +35,8 @@
 {
     // Update the user interface for the detail item.
 
-    BirdSighting *theSighting = self.sighting;
-    
-    static NSDateFormatter *formatter = nil;
-    if (formatter == nil) {
-        formatter = [[NSDateFormatter alloc] init];
-        [formatter setDateStyle:NSDateFormatterMediumStyle];
-    }
-    
-    if (theSighting) {
-        self.birdNameLabel.text = theSighting.name;
-        self.locationLabel.text = theSighting.location;
-        self.dateLabel.text = [formatter stringFromDate:(NSDate *)theSighting];
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
     }
 }
 
