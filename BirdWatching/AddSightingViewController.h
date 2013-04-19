f89ddb2dbbbc04f4fba92243d20d41ceb9c9456e
luyuan_mac <luyuan_mac@luyuan-mactekiMac-mini.local>
2013-04-19 17:48:03 +0800

    Enabling the Addition of New Items (add class)

---
+++ BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by luyuan_mac on 13-4-19.
+//  Copyright (c) 2013年 Eisoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
